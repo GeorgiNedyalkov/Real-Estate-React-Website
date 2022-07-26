5314bda01b8dd6a756ac2e19ff037d63ead78432
Flu3nt <gnedyalkov94@gmail.com>
2022-07-26 15:05:22 +0300

    Developing the Hero section

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 A Real Estate website build using react from this [tutorial](https://www.youtube.com/watch?v=sKs9FiAHSN4)
 
 ## Create and style the Navbar and Header
+
+## Create the Hero Section
---
+++ src/data/SliderData.js
@@ -0,0 +1,70 @@
+import ImageOne from "../images/ImageOne";
+import ImageTwo from "../images/ImageTwo";
+import ImageThree from "../images/ImageThree";
+import ImageFour from "../images/ImageFour";
+// import ImageFive from "../images/ImageFive";
+// import ImageSix from "../images/ImageSix";
+// import ImageSeven from "../images/ImageSeven";
+
+export const SliderData = [
+  {
+    title: "Luxury Villa in Bali, Indonesia",
+    price: "$4,280,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+  {
+    title: "House in San Jose, Costa Rica",
+    price: "$2,860,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageTwo,
+    alt: "House",
+  },
+  {
+    title: "House in Calabasas, California",
+    price: "$8,420,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageThree,
+    alt: "House",
+  },
+  {
+    title: "House in Houston, Texas",
+    price: "$6,660,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageFour,
+    alt: "House",
+  },
+];
+//
+// export const SliderDataTwo = [
+//   {
+// title: "Homes in Las Vegas, Nevada",
+// price: "$4,280,000",
+// path: "/homes",
+// label: "View Home",
+// image: ImageFive,
+// alt: "House",
+//   },
+//   {
+// title: "House in Alajuela, Costa Rica",
+// price: "$5,420,000",
+// path: "/homes",
+// label: "View Home",
+// image: ImageSix,
+// alt: "House",
+//   },
+//   {
+// title: "House in Houston, Texas",
+// price: "$6,660,000",
+// path: "/homes",
+// label: "View Home",
+// image: ImageSeven,
+// alt: "House",
+//   },
+// ];
+//
